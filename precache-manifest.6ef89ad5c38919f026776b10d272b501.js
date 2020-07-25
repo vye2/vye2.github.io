21fd62bccf2a4b7dce202b8a7fcf3b04ea803734
vye2 <vye2@ucsc.edu>
2020-07-24 18:36:44 -0700

    Add files via upload

---
+++ precache-manifest.6ef89ad5c38919f026776b10d272b501.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af95c15f6e40bb1990d19dbaa9bdb9b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0504f62e213447c53d62",
+    "url": "/portfolio/static/css/main.1d2873de.chunk.css"
+  },
+  {
+    "revision": "0e3d190e68567fe2a1a1",
+    "url": "/portfolio/static/js/2.eaec1948.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.eaec1948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0504f62e213447c53d62",
+    "url": "/portfolio/static/js/main.bbf4e0ce.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "51016b716e292663ae598222caf0bcb6",
+    "url": "/portfolio/static/media/ConsumerHealth.51016b71.pdf"
+  },
+  {
+    "revision": "c536087f02534e0421138faebf96fb39",
+    "url": "/portfolio/static/media/FridgeBuddy.c536087f.pdf"
+  },
+  {
+    "revision": "4a874d9f219cac9a7df9765f7d7458a0",
+    "url": "/portfolio/static/media/VisualStimuli.4a874d9f.pdf"
+  },
+  {
+    "revision": "c8f4e2649b667cb1e7569120bd7a468d",
+    "url": "/portfolio/static/media/Ye_Victor_Resume.c8f4e264.pdf"
+  },
+  {
+    "revision": "64ef400f1abb67f9a2efa58e7a171219",
+    "url": "/portfolio/static/media/circleportrait.64ef400f.png"
+  },
+  {
+    "revision": "84a45f3b1e56235dc5081d84c8821b0e",
+    "url": "/portfolio/static/media/clipart.84a45f3b.png"
+  },
+  {
+    "revision": "4a5d4d855d11fae79cc864bdacb2f479",
+    "url": "/portfolio/static/media/fa-brands-400.4a5d4d85.eot"
+  },
+  {
+    "revision": "4c1da237bdae0773309df93b2cd48e09",
+    "url": "/portfolio/static/media/fa-brands-400.4c1da237.ttf"
+  },
+  {
+    "revision": "5734d789b25228cbafc64a58ae971aca",
+    "url": "/portfolio/static/media/fa-brands-400.5734d789.woff"
+  },
+  {
+    "revision": "91a23e8bf2b4b84c39311cb5eb23aaa0",
+    "url": "/portfolio/static/media/fa-brands-400.91a23e8b.woff2"
+  },
+  {
+    "revision": "a5b1d2241ef2aca3aadfa9f52c2e3cab",
+    "url": "/portfolio/static/media/fa-brands-400.a5b1d224.svg"
+  },
+  {
+    "revision": "1ef86349506d95af50c9ddc67efcb56a",
+    "url": "/portfolio/static/media/fa-regular-400.1ef86349.svg"
+  },
+  {
+    "revision": "260be4f29c0b2ce47480afb23f38f237",
+    "url": "/portfolio/static/media/fa-regular-400.260be4f2.ttf"
+  },
+  {
+    "revision": "5c674c9216c06ede2f618aa58ae71116",
+    "url": "/portfolio/static/media/fa-regular-400.5c674c92.woff2"
+  },
+  {
+    "revision": "6b20949b3a679c30d09f64acd5d3317d",
+    "url": "/portfolio/static/media/fa-regular-400.6b20949b.eot"
+  },
+  {
+    "revision": "d44ad00c44e46fd29f6126fa7d888cde",
+    "url": "/portfolio/static/media/fa-regular-400.d44ad00c.woff"
+  },
+  {
+    "revision": "0e19b17d33e4d9cb690576eae576457f",
+    "url": "/portfolio/static/media/fa-solid-900.0e19b17d.svg"
+  },
+  {
+    "revision": "412a43d6840addd683665ec12c30f810",
+    "url": "/portfolio/static/media/fa-solid-900.412a43d6.woff2"
+  },
+  {
+    "revision": "9a1672a8a8d91fbf82c71f451d495253",
+    "url": "/portfolio/static/media/fa-solid-900.9a1672a8.eot"
+  },
+  {
+    "revision": "c65d154888aa166982dac3e72e7380ec",
+    "url": "/portfolio/static/media/fa-solid-900.c65d1548.ttf"
+  },
+  {
+    "revision": "f3a7d3b5880544a91e9a7e6f8f35d4d2",
+    "url": "/portfolio/static/media/fa-solid-900.f3a7d3b5.woff"
+  },
+  {
+    "revision": "174a89ac1a54e35434edb6c3ee474ba1",
+    "url": "/portfolio/static/media/project1.174a89ac.jpg"
+  },
+  {
+    "revision": "a66f707cd4d224d841f35235ad93edaa",
+    "url": "/portfolio/static/media/project2.a66f707c.jpg"
+  },
+  {
+    "revision": "923f05a45d89a7413c8a438e182e33af",
+    "url": "/portfolio/static/media/project3.923f05a4.jpg"
+  },
+  {
+    "revision": "79f5ad3ef76e2bdfe4870def86786092",
+    "url": "/portfolio/static/media/project4.79f5ad3e.jpg"
+  }
+]);
