e3000a50043a2e065c9592675ae00cd2e51ac2ab
vye2 <vye2@ucsc.edu>
2020-08-02 23:26:50 -0700

    change about paragraphs

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6ef89ad5c38919f026776b10d272b501.js"
+  "/portfolio/precache-manifest.1b97a1712414d000bcba15913401e277.js"
 );
 
 self.addEventListener('message', (event) => {
